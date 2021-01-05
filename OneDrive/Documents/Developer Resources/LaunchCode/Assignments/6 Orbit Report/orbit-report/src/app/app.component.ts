82f7a228f9ad1fbbba71dd612d770161e810f6b3
Santi <70606628+santiareh@users.noreply.github.com>
2021-01-04 20:21:09 -0600

    finished step 7

--- OneDrive/Documents/Developer Resources/LaunchCode/Assignments/6 Orbit Report/orbit-report/src/app/app.component.ts
+++ OneDrive/Documents/Developer Resources/LaunchCode/Assignments/6 Orbit Report/orbit-report/src/app/app.component.ts
@@ -23,10 +23,25 @@ export class AppComponent {
         for(let i = 0; i < fetchedSatellites.length; i++){
           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-          satellites = fetchedSatellites[i];
+          satellites = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellites);
         }   
        }.bind(this));
     }.bind(this));
  }
+ //Step 8: Seach class
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+
 }

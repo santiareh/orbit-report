3c30ac742087d2aaf1ff03846c4de80e0da10027
Santi <70606628+santiareh@users.noreply.github.com>
2021-01-05 19:51:13 -0600

    minor fixes DONE

--- OneDrive/Documents/Developer Resources/LaunchCode/Assignments/6 Orbit Report/orbit-report/src/app/orbit-list/orbit-list.component.ts
+++ OneDrive/Documents/Developer Resources/LaunchCode/Assignments/6 Orbit Report/orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -12,7 +12,7 @@ export class OrbitListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  //Step 7: Sort function
   sort(column: string): void {
     // array.sort modifies the array, sorting the items based on the given compare function
     this.satellites.sort(function(a: Satellite, b: Satellite): number {
